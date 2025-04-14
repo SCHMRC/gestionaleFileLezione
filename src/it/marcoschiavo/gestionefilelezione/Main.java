080ec01d4e98210f783f3240a38d0ca2c17f8c76
SCHMRC <schiavo.marco05@gmail.com>
2025-04-14 21:47:50 +0200

    aggiunto il buffer in lettura

--- src/it/marcoschiavo/gestionefilelezione/Main.java
+++ src/it/marcoschiavo/gestionefilelezione/Main.java
@@ -2,6 +2,7 @@ package it.marcoschiavo.gestionefilelezione;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -28,6 +29,12 @@ public class Main {
 		bw.close();
 		fw.close();
 		//TODO: lettura del file
+		FileReader fr = new FileReader(file);
+		//utilizzo il metodo read di un oggetto istanza di FileReader
+		System.out.println(fr.read());
+		//chiudo lo stream in lettura
+		fr.close();
+		
 	}
 
 }
