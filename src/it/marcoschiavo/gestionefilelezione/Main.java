b9b047c53e039a31354dbe76574ba8c57c2d4eee
SCHMRC <schiavo.marco05@gmail.com>
2025-04-14 20:20:42 +0200

    passo 1: creazione del file fisico

--- src/it/marcoschiavo/gestionefilelezione/Main.java
+++ src/it/marcoschiavo/gestionefilelezione/Main.java
@@ -1,10 +1,18 @@
 package it.marcoschiavo.gestionefilelezione;
 
+import java.io.File;
 import java.io.IOException;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		//creo un oggetto di tipo file, NON crea il file fisico nel FileSystem
+		File file = new File("./prova.txt");
+		//controllo che il file non esista
+		if(!file.exists()) {
+			//se non esiste lo creo
+			file.createNewFile();
+		}
 
 	}
 
